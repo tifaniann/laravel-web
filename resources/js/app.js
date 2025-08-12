ff0cfe51b9d8779e54ce1f5a80a46405204a7ee8
Tifani Anasya Putri <tifanianasyaputri9f.39@gmail.com>
2025-08-12 08:14:06 +0700

    crud

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+// import './bootstrap';
+
+// Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// CSS custom kamu
+import '../css/styles.css';
+
+// jQuery & Bootstrap JS
+import $ from 'jquery';
+import 'bootstrap';
+
+// Kalau perlu Popper
+import '@popperjs/core';
+
